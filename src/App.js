eb2d68cb38d1793ffe500f53707797e1b134feb8
The Jobless Coder <thejoblesscoder@gmail.com>
2024-02-29 02:15:05 -0700

    Added webcam support. Added new UseWindowSize hook.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
-import { useState, Suspense } from 'react'
+import { useState, Suspense, useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
+import { Euler } from 'three'
+import Webcam from "react-webcam"
+
 import { Mask, Loader } from "./components"
-import { Euler } from 'three';
+import { useWindowSize } from "./utils/useWindowSize"
+
+const inputResolution = {
+  width: 730 / 4,
+  height: 640 / 4,
+}
+const videoConstraints = {
+  width: inputResolution.width,
+  height: inputResolution.height,
+  facingMode: "user",
+}
+const videoStyle = {
+  position: "fixed",
+  zIndex: 2,
+  top: 0,
+  left: 0
+}
 
 function App() {
   const [rotation, setRotation] = useState(new Euler(0,0,0))
+  const [videoOn, setVideoOn] = useState(true)
+  const [loaded, setLoaded] = useState(false)
+  const [width, height] = useWindowSize();
+  const canvasRef = useRef();
 
-  return (
-    <Canvas style={{background: "grey"}}>
+  const handleVideoLoad = async (videoNode) => {
+    const video = videoNode.target;
+    if (video.readyState !== 4) return;
+    if (loaded) return;
+    //await runDetector(video, canvasRef.current, (data) => {
+    //  console.log(data)
+    //});
+    setLoaded(true);
+  }
+  
+  function toggleVideo(e) {
+    setVideoOn(!videoOn)
+  }
+
+  return (<>
+    <Webcam
+      width={inputResolution.width}
+      height={inputResolution.height}
+      style={{...videoStyle, visibility: !videoOn ? "hidden" : "visible"}}
+      videoConstraints={videoConstraints}
+      onLoadedData={handleVideoLoad}
+    />
+
+    <Canvas ref={canvasRef} style={{background: "linear-gradient(#0094FF, #51B6FF)"}}>
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
       <Suspense fallback={<Loader />}>
-        <Mask scale={1.5} rot={rotation} />
+        <Mask scale={width < 600 ? 1 : 1.5} rot={rotation} />
       </Suspense>
-  </Canvas>
-  );
+    </Canvas>
+
+    <h2 onClick={toggleVideo} style={{position: "fixed", bottom: 0, left: 10, color: 'white'}}>
+      Show Camera <input type='checkbox' onChange={toggleVideo} checked={videoOn} style={{width: 15, height: 15, marginTop: 15}} />
+      <span style={{display: "none"}}>Window size: {width} x {height}</span>
+    </h2>
+  </>);
 }
 
 export default App;
--- src/components/loaders/Loader.js
+++ src/components/loaders/Loader.js
@@ -2,7 +2,7 @@ import { Html, useProgress } from '@react-three/drei'
 
 function Loader() {
   const { progress } = useProgress()
-  return <Html center>{progress} % loaded</Html>
+  return <Html center>{Math.trunc(progress)} % loaded</Html>
 }
 
 export default Loader;
